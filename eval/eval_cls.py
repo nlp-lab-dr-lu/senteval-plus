39e29a26db9eca5302a740db336140baf0ba6a2f
Ali Forooghi <foroogh@delta.cs.uwindsor.ca>
2024-02-29 14:35:05 -0500

    fixed inheritance

--- eval/eval_cls.py
+++ eval/eval_cls.py
@@ -3,93 +3,76 @@ import sys
 import json
 import numpy as np
 import pandas as pd
+import random
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import emb_util
 
 # from whitening import whiten
 from whitening import Whitens
-from validation import InnerKFoldClassifier
+from validation import InnerKFoldMLPClassifier, InnerKFoldClassifier
 from IsoScore import IsoScore
 
-from sklearn.model_selection import StratifiedKFold
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
-import matplotlib
 import matplotlib.pyplot as plt
-from matplotlib import font_manager
 
 class Evaluation:
-    def __init__(self):
-        self.BASE_PATH = 'embeddings/'
-        self.drawcm = True
-        eval_mlp = True
-        classifier = 'mlp' if eval_mlp else 'lr'
-        eval_whitening = True
-        results = []
-        datasets = ["bbbp"] #"mr", "cr", "subj", "mpqa", "trec", "mrpc", "sstf"
-        kfold = 5
-        encoders = [
-            "bert",
-            "all-mpnet-base-v2",
-            # "simcse",
-            "angle-bert",
-            # "angle-llama",
-            # "llama-7B", "llama2-7B",
-            # "text-embedding-3-small"
-        ]
+    def __init__(self, config):
+        """
+            config: Dict
+                EMBEDDINGS_PATH: path to load the embeddings
+                RESULTS_PATH: path to save results
+                drawcm: whether to draw the confusion matrix or not
+                classifier: type of classifier for classification task ['lr', 'rf', 'nv', 'svm']
+                kfold: number of folds for k fold inner classification
+                encoders: list of encoders to evaluate
+                datasets: list of datasets for evaluation ["mr", "cr", "subj", "mpqa", "trec", "mrpc", "sstf", "bbbp"]
+        """
+        self.EMBEDDINGS_PATH = 'embeddings/' if 'EMBEDDINGS_PATH' not in config else config['EMBEDDINGS_PATH']
+        self.RESULTS_PATH = 'results/' if 'RESULTS_PATH' not in config else config['RESULTS_PATH']
+        self.drawcm = False if 'drawcm' not in config else config['drawcm']
+        self.classifier = 'mlp' if 'classifier' not in config else config['classifier']
+        self.eval_whitening = True
+        self.kfold = 5
+        list_of_encoders = ["bert", "all-mpnet-base-v2", "simcse", "angle-bert", "angle-llama", "llama-7B", "llama2-7B", "text-embedding-3-small"]
+        self.encoders = list_of_encoders if 'encoders' not in config else config['encoders']
 
-        for dataset in datasets:
-            for encoder in encoders:
-                print(f"<<evaluating {dataset} with {encoder} with {classifier}>>")
+        if 'datasets' not in config:
+            raise Exception("No datasets decleard")
+        else:
+            self.datasets = config['datasets']
+
+        self.results = []
+
+    def run(self):
+        for dataset in self.datasets:
+            for encoder in self.encoders:
+                print(f"<<evaluating {dataset} with {encoder} with {self.classifier}>>")
                 X, y, nclasses = self.load_data(dataset, encoder)
 
                 IScore = IsoScore.IsoScore(X)
 
-                acc, acc_list, f1_w = self.sentEval(X, y, kfold, classifier, nclasses, dataset, 'No Whitening', encoder)
+                acc, acc_list, f1_w = self.sentEval(X, y, self.kfold, self.classifier, nclasses, dataset, 'No Whitening', encoder)
                 print(f'\twhitening method:  : {acc} : {f1_w}')
                 result = {
                     'dataset': dataset,
                     'encoder': encoder,
-                    'classfier': classifier,
+                    'classfier': self.classifier,
                     'whitening': '',
                     'accuracy': acc,
                     'f1_weighted': f1_w,
                     'IScore': IScore.item(),
                     'accuracy_list': acc_list,
-                    'kfold': kfold
+                    'kfold': self.kfold
                 }
-                results.append(result)
+                self.results.append(result)
 
-                if eval_whitening:
-                    whitenings_methods = ['pca', 'zca', 'zca_cor', 'pca_cor', 'cholesky']
-                    for method in whitenings_methods:
-                        trf = Whitens().fit(X, method = method)
-                        X_whitened = trf.transform(X) # X is whitened
-                        acc, acc_list, f1_w = self.sentEval(X_whitened, y, kfold, classifier, nclasses, dataset, method, encoder)
-                        print(f'\twhitening method: {method} : {acc} : {f1_w}')
-                        IScore = IsoScore.IsoScore(X_whitened)
-
-                        method = 'zca-cor' if method == 'zca_cor' else method # make sure to be consistant in file names
-                        method = 'pca-cor' if method == 'pca_cor' else method # make sure to be consistant in file names
-                        result = {
-                            'dataset': dataset,
-                            'encoder': encoder,
-                            'classfier': classifier,
-                            'whitening': method,
-                            'accuracy': acc,
-                            'f1_weighted': f1_w,
-                            'IScore': IScore.item(),
-                            'accuracy_list': acc_list,
-                            'kfold': kfold
-                        }
-                        results.append(result)
-
-                json_object = json.dumps(results, indent=4)
-                with open(f"results/{dataset}_eval/{classifier}_eval_results3.json", "w") as outfile:
+                json_object = json.dumps(self.results, indent=4)
+                with open(f"{self.RESULTS_PATH}/{dataset}_eval/{self.classifier}_eval_results3.json", "w") as outfile:
                     outfile.write(json_object)
-
+        return self.results
+    
     def sentEval(self, X, y, kfold, classifier, nclasses, dataset, whitening_method, encoder):
         if(classifier == 'mlp'):
             classifier = {
@@ -101,48 +84,32 @@ class Evaluation:
             }
             config = {
                 'nclasses': nclasses,
-                'seed': 2,
+                'seed': random.randint(1, 100),
                 'usepytorch': True,
                 'classifier': classifier,
                 'nhid': classifier['nhid'],
                 'kfold': kfold,
-                'drawcm': True
+                'drawcm': False
             }
-            clf = InnerKFoldClassifier(X, y, config)
+            clf = InnerKFoldMLPClassifier(X, y, config)
             dev_accuracy, test_accuracy, test_f1_w, testresults_acc, cm_data = clf.run()
             if(self.drawcm):
                 self.draw_cm(cm_data, dataset, whitening_method, encoder)
+        elif(classifier in ['lr', 'rf', 'svm', 'nb']):
+            config = {
+                'nclasses': nclasses,
+                'seed': random.randint(1, 100),
+                'classifier': classifier,
+                'kfold': kfold,
+                'drawcm': False
+            }
+            clf = InnerKFoldClassifier(X, y, config)
         else:
             raise Exception("unknown classifier")
 
         return test_accuracy, testresults_acc, test_f1_w
 
-    def draw_cm(self, cm_data, dataset, whitening_method, encoder, download_font=False):
-        if download_font:
-            # add the font to Matplotlib
-            # create the directory for fonts if it doesn't exist
-            font_dir = "font/"
-            os.makedirs(font_dir, exist_ok=True)
-
-            # Download the font
-            font_url = "http://foroogh.myweb.cs.uwindsor.ca/Times_New_Roman.ttf"
-            font_path = os.path.join(font_dir, "times_new_roman.ttf")
-            if not os.path.exists(font_path):
-                os.system(f"wget -P {font_dir} {font_url}")
-
-        font_files = font_manager.findSystemFonts(fontpaths=['font/'])
-        for font_file in font_files:
-            font_manager.fontManager.addfont(font_file)
-
-        # Verify the font is recognized by Matplotlib
-        font_name = "Times New Roman"
-        if font_name in font_manager.get_font_names():
-            print(f"'{font_name}' font successfully added.")
-            # Set default font to Times New Roman
-            matplotlib.rc('font', family=font_name)
-        else:
-            print(f"'{font_name}' font not found. Please check the font installation.")
-
+    def draw_cm(self, cm_data, dataset, whitening_method, encoder):
         cm = confusion_matrix(cm_data['y_test'], cm_data['y_pred'], labels=np.unique(cm_data['y_test']))
         disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=np.unique(cm_data['y_test']))
         fig, ax = plt.subplots(figsize=(4, 4))
@@ -150,22 +117,22 @@ class Evaluation:
         ax.set_title(whitening_method.upper())
         plt.rcParams.update({'font.size': 30})
         disp.plot(ax=ax, cmap='Blues', colorbar=False, values_format='d')
-        path = f'results/{dataset}_eval/cm/{encoder}-{whitening_method}-cm.pdf'
+        path = f'{self.RESULTS_PATH}/{dataset}_eval/cm/{encoder}-{whitening_method}-cm.pdf'
         print('svaing cm plot',path)
         fig.savefig(path, format='pdf')
 
     def load_data(self, dataset_name, encoder_name):
+        path = f'{self.EMBEDDINGS_PATH}{dataset_name}/{encoder_name}_{dataset_name}'
         if (dataset_name in emb_util.splitted_datasets):
-            base_path = f'{self.BASE_PATH}{dataset_name}/{encoder_name}_{dataset_name}'
-            data_train = pd.read_csv(base_path+'_train_embeddings.csv' ,sep='\t')
-            data_test = pd.read_csv(base_path+'_test_embeddings.csv' ,sep='\t')
+            data_train = pd.read_csv(path+'_train_embeddings.csv' ,sep='\t')
+            data_test = pd.read_csv(path+'_test_embeddings.csv' ,sep='\t')
             data = pd.concat([data_train, data_test], axis=0)
         elif(dataset_name in emb_util.unsplitted_datasets):
-            path = f'{self.BASE_PATH}{dataset_name}/{encoder_name}_{dataset_name}_embeddings.csv'
-            data = pd.read_csv(path, sep='\t')
+            data = pd.read_csv(path+'_embeddings.csv', sep='\t')
         else:
-            raise Exception("unknown dataset")
+            raise Exception("Could'nt find embeddings file")
 
+        data = emb_util.check_and_reorder_dataframe(data)       
         # get labels only
         labels = []
         for i, data_row in data.iterrows():
@@ -179,6 +146,3 @@ class Evaluation:
         nclasses = len(classes)
 
         return X, y, nclasses
-
-if __name__ == "__main__":
-    eval = Evaluation()
--- eval/validation.py
+++ eval/validation.py
@@ -10,6 +10,11 @@ assert (sklearn.__version__ >= "0.18.0"), \
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import f1_score, accuracy_score #For the binary case will return F1 score for pos_label
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 
 def get_classif_name(classifier_config, usepytorch):
     if not usepytorch:
@@ -23,10 +28,7 @@ def get_classif_name(classifier_config, usepytorch):
 
 
 # Pytorch version
-class InnerKFoldClassifier(object):
-    """
-    (train) split classifier : InnerKfold.
-    """
+class InnerKFoldMLPClassifier(object):
 
     def __init__(self, X, y, config):
         self.X = X
@@ -40,12 +42,10 @@ class InnerKFoldClassifier(object):
         self.usepytorch = config['usepytorch']
         self.classifier_config = config['classifier']
         self.modelname = get_classif_name(self.classifier_config, self.usepytorch)
-        self.drawcm = False if 'drawcm' not in config else config['drawcm']
         self.k = 5 if 'kfold' not in config else config['kfold']
 
     def run(self):
-        logging.info('Training {0} with (inner) {1}-fold cross-validation'
-                     .format(self.modelname, self.k))
+        logging.info('Training {0} with (inner) {1}-fold cross-validation'.format(self.modelname, self.k))
 
         regs = [10 ** t for t in range(-5, -1)] if self.usepytorch else \
             [2 ** t for t in range(-2, 4, 1)]
@@ -63,37 +63,95 @@ class InnerKFoldClassifier(object):
                 for inner_train_idx, inner_test_idx in innerskf.split(X_train, y_train):
                     X_in_train, X_in_test = X_train[inner_train_idx], X_train[inner_test_idx]
                     y_in_train, y_in_test = y_train[inner_train_idx], y_train[inner_test_idx]
-                    if self.usepytorch:
-                        clf = MLP(self.classifier_config, inputdim=self.featdim,
-                                  nclasses=self.nclasses, l2reg=reg,
-                                  seed=self.seed)
-                        clf.fit(X_in_train, y_in_train,
-                                validation_data=(X_in_test, y_in_test))
-                    else:
-                        clf = LogisticRegression(C=reg, random_state=self.seed)
-                        clf.fit(X_in_train, y_in_train)
+                    clf = MLP(self.classifier_config, 
+                              inputdim=self.featdim,
+                              nclasses=self.nclasses, 
+                              l2reg=reg,
+                              seed=self.seed)
+                    clf.fit(X_in_train, y_in_train,validation_data=(X_in_test, y_in_test))
+                 
                     regscores.append(clf.score(X_in_test, y_in_test))
                 scores.append(round(100 * np.mean(regscores), 2))
             optreg = regs[np.argmax(scores)]
-            logging.info('Best param found at split {0}: l2reg = {1} \
-                with score {2}'.format(count, optreg, np.max(scores)))
+            logging.info('Best param found at split {0}: l2reg = {1} with score {2}'.format(count, optreg, np.max(scores)))
             self.devresults.append(np.max(scores))
 
-            if self.usepytorch:
-                clf = MLP(self.classifier_config, inputdim=self.featdim,
-                          nclasses=self.nclasses, l2reg=optreg,
-                          seed=self.seed)
-                clf.fit(X_train, y_train, validation_split=0.05)
-            else:
-                clf = LogisticRegression(C=optreg, random_state=self.seed)
-                clf.fit(X_train, y_train)
+            clf = MLP(self.classifier_config, 
+                      inputdim=self.featdim,
+                      nclasses=self.nclasses, 
+                      l2reg=optreg,
+                      seed=self.seed)
+            clf.fit(X_train, y_train, validation_split=0.05)
+            y_pred = clf.predict(X_test)
+            self.testaccs.append(round(100 * clf.score(X_test, y_test), 2))
+            self.testf1s.append(round(100 * f1_score(y_test, y_pred, average='weighted')))
+            cm_data = {'y_test': y_test, 'y_pred': y_pred}
 
+        devaccuracy = round(np.mean(self.devresults), 2)
+        testaccuracy = round(np.mean(self.testaccs), 2)
+        testf1 = round(np.mean(self.testf1s), 2)
+
+        return devaccuracy, testaccuracy, testf1, self.testf1s, cm_data
+    
+class InnerKFoldClassifier(object):
+
+    def __init__(self, X, y, config):
+        self.X = X
+        self.y = y
+        self.featdim = X.shape[1]
+        self.nclasses = config['nclasses']
+        self.seed = config['seed']
+        self.devresults = []
+        self.testaccs = []
+        self.testf1s = []
+        self.usepytorch = config['usepytorch']
+        self.classifier_config = config['classifier']
+        self.modelname = get_classif_name(self.classifier_config, self.usepytorch)
+        self.k = 5 if 'kfold' not in config else config['kfold']
+
+    def run(self):
+        logging.info('Training {0} with (inner) {1}-fold cross-validation'.format(self.modelname, self.k))
+
+        skf = StratifiedKFold(n_splits=self.k, shuffle=True, random_state=1111)
+        innerskf = StratifiedKFold(n_splits=self.k, shuffle=True, random_state=1111)
+        count = 0
+        for train_idx, test_idx in skf.split(self.X, self.y):
+            count += 1
+            X_train, X_test = self.X[train_idx], self.X[test_idx]
+            y_train, y_test = self.y[train_idx], self.y[test_idx]
+            scores = []
+
+            if self.modelname == 'lr':
+                # logistic regression will have hypertuning
+                regs = [2 ** t for t in range(-2, 4, 1)]
+                scores = []
+                for reg in regs:
+                    regscores = []
+                    for inner_train_idx, inner_test_idx in innerskf.split(X_train, y_train):
+                        X_in_train, X_in_test = X_train[inner_train_idx], X_train[inner_test_idx]
+                        y_in_train, y_in_test = y_train[inner_train_idx], y_train[inner_test_idx]
+                        clf = LogisticRegression(C=reg, random_state=self.seed)
+                        clf.fit(X_in_train, y_in_train)
+                        regscores.append(clf.score(X_in_test, y_in_test))
+                    scores.append(round(100 * np.mean(regscores), 2))
+                optreg = regs[np.argmax(scores)]
+                logging.info('Best param found at split {0}: l2reg = {1} with score {2}'.format(count, optreg, np.max(scores)))
+                self.devresults.append(np.max(scores))
+                clf = LogisticRegression(C=optreg, random_state=self.seed)
+            elif self.modelname == 'svm':
+                clf = make_pipeline(StandardScaler(), SVC(gamma='auto'), random_state=self.seed)
+            elif self.modelname == 'rf':
+                clf = RandomForestClassifier(max_depth=5, random_state=self.seed)
+            elif self.modelname == 'nb':
+                clf = GaussianNB(random_state=self.seed)
+            else:
+                raise Exception("unknown classifier")
+
+            clf.fit(X_train, y_train)
 
             self.testaccs.append(round(100 * clf.score(X_test, y_test), 2))
             y_pred = clf.predict(X_test)
             self.testf1s.append(round(100 * f1_score(y_test, y_pred, average='weighted')))
-
-            y_pred = clf.predict(X_test)
             cm_data = {'y_test': y_test, 'y_pred': y_pred}
 
         devaccuracy = round(np.mean(self.devresults), 2)
